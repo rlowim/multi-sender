8bc42cbcaa41ac35bc07058c61962baacb164065
rlowim <rlowim@tutanota.com>
2024-03-27 04:44:41 +0100

    test: add a test for sending transactions from different addresses

--- test/MultiSender.test.js
+++ test/MultiSender.test.js
@@ -4,9 +4,9 @@ const { loadFixture } = require("@nomicfoundation/hardhat-toolbox/network-helper
 
 describe("MultiSender contract", function() {
   async function deployMultiSenderFixture() {
-    const [ signer, acc1, acc2 ] = await ethers.getSigners();
+    const [ signer, acc1, acc2, acc3 ] = await ethers.getSigners();
     const contract = await ethers.deployContract("MultiSender");
-    return { contract, signer, acc1, acc2 };
+    return { contract, signer, acc1, acc2, acc3 };
   }
   describe("Deployment", function() {
     it("Shoud be deployed on a address", async function() {
@@ -36,6 +36,11 @@ describe("MultiSender contract", function() {
     it("Should revert with the right error if if not enough funds were sent", async function() {
       const { contract, acc1, acc2 } = await loadFixture(deployMultiSenderFixture);
       await expect(contract.multiSend(100000, [acc1.address, acc2.address], {value: 1000})).to.be.revertedWith("Not enough funds");
+    });
+    it("Should be possible to send ethers from any account", async function() {
+      const { contract, signer, acc1, acc2, acc3 } = await loadFixture(deployMultiSenderFixture);
+      await expect(contract.connect(acc1).multiSend(100000, [acc2.address, acc3.address], {value: 1000000})).to.not.be.reverted;
+      await expect(contract.connect(acc2).multiSend(100000, [signer.address, acc3.address], {value: 1000000})).to.not.be.reverted;
     });
   });
 });
